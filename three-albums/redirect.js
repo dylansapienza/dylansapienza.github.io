5bae32d1c1a450a10d02f9e824d198beb6a8abcd
Dylan Sapienza <dylsapienza12@gmail.com>
2026-08-17 10:35:08 -0400

    Activate Three Albums App Store redirect

--- three-albums/redirect.js
+++ three-albums/redirect.js
@@ -1,2 +1,2 @@
 // Add the App Store or TestFlight URL between the quotes to enable the redirect.
-window.THREE_ALBUMS_DESTINATION = "";
+window.THREE_ALBUMS_DESTINATION = "https://apps.apple.com/us/app/three-albums/id6797140255";
